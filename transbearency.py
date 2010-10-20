6b697fe7efa06c65401c336fe9de10b02ac3d990
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-10-20 09:31:07 -0400

    do full templating

--- transbearency.py
+++ transbearency.py
@@ -2,14 +2,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = False
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
     
 @app.route('/thanks')
 def thanks():
     return render_template('thanks.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
